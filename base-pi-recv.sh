e38150c5bc48d211782bee710ebdc20a22c4ff20
Garrett Weaver <wagarrettweaver@gmail.com>
2018-07-24 20:29:37 -0700

    Got audio rcv to work on the base pi.

--- base-pi-recv.sh
+++ base-pi-recv.sh
@@ -16,5 +16,5 @@ done
 
 gst-launch-1.0 -v udpsrc \
 	multicast-group=$MULTI_CAST_IP auto-multicast=true port=$REMOTE_VIDEO_PORT caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtpjitterbuffer ! rtph264depay ! h264parse ! omxh264dec ! queue ! videoconvert ! queue ! fbdevsink sync=false \
-	udpsrc multicast-group=$MULTI_CAST_IP port=$REMOTE_AUDIO_PORT ! application/x-rtp, media=audio, clock-rate=8000, encoding-name=PCMU ! rtpjitterbuffer ! rtppcmudepay ! queue ! mulawdec ! queue ! audioconvert ! autoaudiosink sync=false 
+	udpsrc multicast-group=$MULTI_CAST_IP port=$REMOTE_AUDIO_PORT ! application/x-rtp, media=audio, clock-rate=8000, encoding-name=PCMU ! rtpjitterbuffer ! rtppcmudepay ! queue ! mulawdec ! queue ! audioconvert ! alsasink sync=false 
 
